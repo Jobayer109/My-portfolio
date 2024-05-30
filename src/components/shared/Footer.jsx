21f6081e7db67c9e0ceac83c53e96ebc7de23249
user.email <jobayer.ahmed109@gmail.com>
2024-05-30 14:39:40 +0600

    Footer icon color change on hover

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -9,39 +9,39 @@ import {
 
 const Footer = () => {
   return (
-    <footer className="footer footer-center p-6  md:mt-12 lg:mt-12 xl:mt-14  text-base-content">
+    <footer className="footer footer-center text-center p-6  md:mt-12 lg:mt-12 xl:mt-14  text-base-content">
       <div>
-        <div className="flex items-center justify-around">
+        <div className="flex items-center justify-around transition-all">
           <a
             href="https://www.linkedin.com/in/jobayer109/"
             target={"_blank"}
             rel="noreferrer"
           >
-            <FaLinkedin className=" ml-6  text-2xl hover:text-white" />
+            <FaLinkedin className=" ml-6  text-2xl hover:fill-amber-400" />
           </a>
           <a
             href="https://github.com/Jobayer109"
             target={"_blank"}
             rel="noreferrer"
           >
-            <FaGithub className=" ml-6 text-2xl hover:text-white" />
+            <FaGithub className=" ml-6 text-2xl hover:fill-amber-400" />
           </a>
           <a
             href="https://www.facebook.com/jobayer1995/"
             target={"_blank"}
             rel="noreferrer"
           >
-            <FaFacebook className=" ml-6  text-2xl hover:text-white" />
+            <FaFacebook className=" ml-6  text-2xl hover:fill-amber-400" />
           </a>
           <a href="/contact">
-            <FaEnvelope className=" ml-6 text-2xl hover:text-white" />
+            <FaEnvelope className=" ml-6 text-2xl hover:fill-amber-400" />
           </a>
           <a href="/">
-            <FaPhone className=" ml-6  text-2xl hover:text-white" />
+            <FaPhone className=" ml-6  text-2xl hover:fill-amber-400" />
           </a>
         </div>
-        <p className="text-gray-400">
-          Copyright © 2022 - All right reserved by Jobayer Ahmed
+        <p className="text-gray-400 text-[14px] ">
+          Copyright © 2024 - All right reserved by Jobayer
         </p>
       </div>
     </footer>
