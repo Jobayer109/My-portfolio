50b7212f799cdae7401bf5349fee38261df67a28
user.email <jobayer.ahmed109@gmail.com>
2024-05-30 07:09:23 +0600

    Card's Visual Hierarchy resolved

--- src/components/Home/Projects/Project.jsx
+++ src/components/Home/Projects/Project.jsx
@@ -11,14 +11,14 @@ const Project = ({ project }) => {
     >
       <div className="card-body ">
         <a target="_blank" href={link} rel="noreferrer">
-          <div className="flex items-center justify-start">
-            <h2 className="card-title text-amber-400">{title}</h2>
-            <HiExternalLink className="text-2xl text-white" />
+          <div className="flex items-center justify-start gap-1">
+            <h2 className="card-title text-2xl text-amber-400">{title}</h2>
+            <HiExternalLink className="text-xl text-white" />
           </div>
         </a>
         <p className="text-gray-400 text-sm">{type}</p>
         <Link to={`/project/${_id}`}>
-          <button className=" border mt-2 px-3 py-1 border-amber-400 rounded-md  hover:bg-gray-800 hover:text-white  transition  duration-300 ease-in-out">
+          <button className=" border mt-2 px-3 py-1 border-amber-400 rounded-md  hover:bg-gray-800 text-amber-100 hover:text-white  transition  duration-300 ease-in-out">
             View details
           </button>
         </Link>
